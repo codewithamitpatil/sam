3f90d93c4465bc7c1256cd28c36a801ca5146b4e
codewithamitpatil <pamit8831@gmail.com>
2022-08-19 18:29:02 +0530

    aaa

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const port = 5000;
+const express = require( 'express' );
+const cors = require( 'cors' );
+const path = require( 'path' );
+const fs = require( 'fs' );
+
+
+// if (fs.existsSync(videoPath)) {
+//   // path exists
+//   console.log("exists:", videoPath);
+//   console.log('size ==>',videoSize)
+
+// } else {
+//   console.log("DOES NOT exist:", path);
+// }
+
+
+process.env.PORT = 5001;
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors('*'));
+app.use( cors( '*' ) );
 
-app.get('',async(req,res)=>{
-    res.send("welcome to home")
-})
-
-
-app.get('/a/:key',async(req,res)=>{
-    res.send(`message ===> ${req.params.key}`)
-})
-
-app.use('*',async(req,res)=>{
-    res.send("url nor found");
-})
+app.get( '', async ( req, res ) => {
+    res.send( process.env )
+} )
 
 
-app.listen(port,()=>{
-    console.log(`Server is running at port => ${port}`);
-})
+app.get( '/a/:key', async ( req, res ) => {
+    res.send( `message ===> ${ req.params.key }` )
+} )
+
+app.use( '*', async ( req, res ) => {
+    res.send( "url nor found" );
+} )
+
+
+app.listen( port, () => {
+    console.log( `Server is running at port => ${ port }` );
+} )
